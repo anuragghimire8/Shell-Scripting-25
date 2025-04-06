3de2bf058629d6506b66d00c581372720dfb236f
anuragghimire8 <tankaghimire3@gmail.com>
2025-04-06 11:17:54 +0545

    first day of scripting

---
+++ day01/first.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Hello! This is my first shell scripting that iam writing now.
+
+echo "Anurag:Hello Guys!My name is Anurag Ghimire.Iam 24 years old.Iam from Parsa,Chitwan,Nepal"
+echo "Anurag:Halo ! Mien name ist Anurag Ghimire.Ich bin vierundzwanzig Jahre alt.Ich Komme aus Parsa,Chitwan,Nepal"
